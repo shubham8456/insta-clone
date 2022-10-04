4c966c43dda2d66cd131bc08e952ba4c06370b74
shubham8456 <shubham8456@gmail.com>
2022-10-04 16:21:41 +0530

    made ui changes and redirection work

--- instagram/config/routes.rb
+++ instagram/config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  
+  get '/homepage' => 'public#homepage'
+
   # Defines the root path route ("/")
-  root to: "public#homepage"
+  devise_scope :user do
+    root 'users/sessions#new'
+  end
 end
