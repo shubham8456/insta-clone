985e611e59df6f9c826cdf187173cc2d700102c0
shubham8456 <shubham8456@gmail.com>
2022-10-05 10:19:15 +0530

    fixed things after doing a rollback

--- instagram/app/controllers/public_controller.rb
+++ instagram/app/controllers/public_controller.rb
@@ -1,6 +1,12 @@
 class PublicController < ApplicationController
+    before_action :authenticate_user!
 
     def homepage
+        #user feed
     end
+    
+    def show
+        #user profile
+    end 
     
 end
