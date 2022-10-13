ed6b5fe76627426aefc2d68d7fa9f3f3a8706a98
shubham8456 <shubham8456@gmail.com>
2022-10-13 15:12:33 +0530

    added follow feature

--- instagram/app/controllers/follows_controller.rb
+++ instagram/app/controllers/follows_controller.rb
@@ -1,18 +1,32 @@
 class FollowsController < ApplicationController
-    before_action authenticate_user!, only: [:create, :destroy]
+    before_action :authenticate_user!, only: [:create, :destroy]
     before_action :find_user
-    before_action :find_following, only: [:destroy]
+    before_action :already_followed, only: [:destroy]
 
     def create
-        @follow = Follow.new(follow_params)
+        if !already_followed && @user != current_user
+            f = Follow.new
+            f.followed_user_id = @user.id
+            f.follower_id = current_user.id
+            f.save
+        end
+        redirect_to homepage_path
     end
 
     def destroy
+        if already_followed
+            Follow.where(follower_id: current_user.id, followed_user_id: @user).first.destroy
+        end
+        redirect_to homepage_path
     end
 
     private
 
     def find_user
         @user = Post.find(params[:user_id])
+    end
+
+    def already_followed
+        Follow.where(follower_id: current_user.id, followed_user_id: params[:user_id]).exists?
     end
 end
